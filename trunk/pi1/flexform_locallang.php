7d7977d71dad971180c8c0e2733e382df6f68c2a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-21 22:29:32 +0000

    2.0.0 - Official release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/quickshop_installer@59510 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ trunk/pi1/flexform_locallang.php
@@ -0,0 +1,31 @@
+<?php
+$LOCAL_LANG = array(
+  'default' => array(
+    'sheet_install'                         => 'Installation',
+    'sheet_install.tutorial'                => 'Tutorial',
+    'sheet_install.tutorial.0'              => 'Quick Shop - Installer: http://typo3.org/extensions/repository/view/quickshop_installer/current/',
+    'sheet_install.install_case'            => 'Installation',
+    'sheet_install.install_case.0'          => 'disabled',
+    'sheet_install.install_case.1'          => 'Quick Shop with website template',
+    'sheet_install.install_case.2'          => 'Quick Shop only',
+    'sheet_install.backend_user'            => 'Backend user (will be the owner of all new pages)',
+    'sheet_install.host'                    => 'The host of your shop (with http://...). Leave it empty, if the frontend URL should used. ',
+    'sheet_install.website_title'           => 'Title of your website.',
+    'sheet_install.mail_subject'            => 'Subject of confirmation mails - required',
+    'sheet_install.mail_default_recipient'  => 'E-mail of the shop owner (is the sender of e-mails to customers) - required',
+  ),
+  'de' => array(
+    'sheet_install'                         => 'Installation',
+    'sheet_install.tutorial'                => 'Tutorial',
+    'sheet_install.tutorial.0'              => 'Quick Shop - Installer: http://typo3.org/extensions/repository/view/quickshop_installer/current/',
+    'sheet_install.install_case'            => 'Installation',
+    'sheet_install.install_case.0'          => 'Deaktiviert',
+    'sheet_install.install_case.1'          => 'Quick Shop mit Website Template',
+    'sheet_install.install_case.2'          => 'Quick Shop',
+    'sheet_install.backend_user'            => 'Backend Benutzer (wird der Eigentuemer aller neuen Seiten des Quick Shops)',
+    'sheet_install.host'                    => 'Der Host Deines Shops (mit http://...). Lass das Feld leer, wenn Du die Frontend URL verwenden willst. ',
+    'sheet_install.mail_subject'            => 'Betreff der Bestaetigungs-Mails - Pflichtfeld',
+    'sheet_install.mail_default_recipient'  => 'Mail des Eigentuemers des Shops (das ist auch die Absender-Mail fuer Mails an Kunden) - Pflichtfeld',
+  ),
+);
+?>
