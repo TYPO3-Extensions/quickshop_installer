060c452ac6aaee52f933b34797685fa92a52ae6e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-21 22:29:32 +0000

    2.0.0 - Official release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/quickshop_installer/trunk@59510 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,52 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "quickshop_installer".
+#
+# Auto generated 07-02-2011 23:01
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Quick Shop - Installer',
+	'description' => 'The installer installs Quick Shop and Quick Shop Template. Installation is out of the box. It is a one click installation.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '2.0.0',
+	'dependencies' => 'browser,powermail,quick_shop,wt_cart',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Dirk Wildt (Die Netzmacher)',
+	'author_email' => 'http://wildt.at.die-netzmacher.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'browser' => '3.6.1-',
+			'powermail' => '1.5.7-',
+			'quick_shop' => '1.4.0-',
+			'wt_cart' => '1.2.1-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:28:{s:9:"ChangeLog";s:4:"df7f";s:12:"ext_icon.gif";s:4:"2501";s:17:"ext_localconf.php";s:4:"1028";s:14:"ext_tables.php";s:4:"9454";s:16:"locallang_db.xml";s:4:"f81f";s:14:"doc/manual.pdf";s:4:"d7a6";s:14:"doc/manual.sxw";s:4:"77ad";s:39:"pi1/class.tx_quickshopinstaller_pi1.php";s:4:"dea6";s:16:"pi1/flexform.xml";s:4:"4857";s:26:"pi1/flexform_locallang.php";s:4:"0083";s:27:"pi1/flexform_sheet_sDEF.xml";s:4:"1949";s:17:"pi1/locallang.xml";s:4:"12e5";s:37:"res/images/22x22/application-exit.png";s:4:"8369";s:42:"res/images/22x22/appointment-recurring.png";s:4:"84e7";s:34:"res/images/22x22/dialog-cancel.png";s:4:"9a5c";s:33:"res/images/22x22/dialog-close.png";s:4:"6063";s:33:"res/images/22x22/dialog-error.png";s:4:"4343";s:39:"res/images/22x22/dialog-information.png";s:4:"959e";s:36:"res/images/22x22/dialog-ok-apply.png";s:4:"a6e0";s:35:"res/images/22x22/dialog-warning.png";s:4:"0eab";s:29:"res/images/22x22/download.png";s:4:"1818";s:32:"res/images/22x22/edit-delete.png";s:4:"7dda";s:44:"res/images/products/book_###TIMESTAMP###.jpg";s:4:"d120";s:47:"res/images/products/capBlue_###TIMESTAMP###.jpg";s:4:"e170";s:48:"res/images/products/capGreen_###TIMESTAMP###.jpg";s:4:"837e";s:46:"res/images/products/capRed_###TIMESTAMP###.jpg";s:4:"00e8";s:43:"res/images/products/cup_###TIMESTAMP###.jpg";s:4:"e418";s:48:"res/images/products/pullover_###TIMESTAMP###.jpg";s:4:"e1ab";}',
+	'suggests' => array(
+	),
+);
+
+?>
