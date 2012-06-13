6bb18d772528ef415f6a91fb29fba19a482595dd
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-13 13:15:40 +0000

    2.1.1 +++ Simplifying Installer
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/quickshop_installer/trunk@63398 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,27 +3,50 @@ if (!defined ('TYPO3_MODE'))  die ('Access denied.');
 
 
 
-  ///////////////////////////////////////////////////////////
-  //
-  // Plugin general configuration
-  
+    ///////////////////////////////////////////////////////////
+    //
+    // INDEX
+
+    // Plugin general configuration
+    // Plugin 1 configuration
+    // Add pagetree icons
+
+
+
+    ///////////////////////////////////////////////////////////
+    //
+    // Plugin general configuration
+
   t3lib_div::loadTCA('tt_content');
-  // Plugin general configuration
+    // Plugin general configuration
 
 
 
-  ///////////////////////////////////////////////////////////
-  //
-  // Plugin 1 configuration
-  
+    ///////////////////////////////////////////////////////////
+    //
+    // Plugin 1 configuration
+
   $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages,recursive';
-  // Remove the default tt_content fields layout, select_key, pages and recursive.
+    // Remove the default tt_content fields layout, select_key, pages and recursive.
   $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
-  // Display the field pi_flexform
+    // Display the field pi_flexform
   t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/pi1/flexform.xml');
-  // Register our file with the flexform structure
+    // Register our file with the flexform structure
   t3lib_extMgm::addPlugin(array('LLL:EXT:quickshop_installer/pi1/locallang.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1', 'EXT:quickshop_installer/ext_icon.gif'),'list_type');
-  // Add the Flexform to the Plugin List
-  // Plugin 1 configuration
+    // Add the Flexform to the Plugin List
+    // Plugin 1 configuration
+
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    // 
+    // Add pagetree icons
+
+  $TCA['pages']['columns']['module']['config']['items'][] = 
+    array('Quick Shop: Installer', 'qs_inst', t3lib_extMgm::extRelPath($_EXTKEY).'ext_icons/ext_icon_blink.gif');
+  //$ICON_TYPES['org_inst'] = 
+  //  array('icon' => t3lib_extMgm::extRelPath($_EXTKEY).'ext_icons/ext_icon_blink.gif');
+  t3lib_SpriteManager::addTcaTypeIcon('pages', 'contains-qs_inst', '../typo3conf/ext/quickshop_installer/ext_icons/ext_icon_blink.gif');
+    // Add pagetree icons
 
 ?>
---
+++ lib/class.tx_quickshop_installer_extmanager.php
@@ -0,0 +1,419 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 - Dirk Wildt <http://wildt.at.die-netzmacher.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.quickshop/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+* Class provides methods for the extension manager.
+*
+* @author    Dirk Wildt <http://wildt.at.die-netzmacher.de>
+* @package    TYPO3
+* @subpackage    quickshop
+* @version 2.1.1
+* @since 2.1.1
+*/
+
+
+  /**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ *
+ *
+ *   49: class tx_quickshop_installer_extmanager
+ *   67:     function promptQuickstart()
+ *
+ * TOTAL FUNCTIONS: 2
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
+ */
+class tx_quickshop_installer_extmanager
+{
+  var $int_pageUid  = null;
+  var $str_llStatic = 'en';
+
+
+
+
+
+
+
+
+  /**
+ * initialPage(): Displays a prompt with the current state.
+ *                If the user enabled the adding of the installer page,
+ *                the installer page will be added in the database.
+ *
+ * @return  string    message wrapped in HTML
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+  function initialPage()
+  {
+//.message-notice
+//.message-information
+//.message-ok
+//.message-warning
+//.message-error
+
+    $str_prompt = null;
+    // 120613, dwildt, 1-
+    //$confArr    = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['quickshop_installer']);
+    // 120613, dwildt, 1+
+    $confArr    = $_POST['data'];
+    //var_dump( $_POST['data'] );
+    $llStatic   = $confArr['LLstatic'];
+
+
+
+      /////////////////////////////////////////////////////////
+      //
+      // Default prompt
+
+      // 120613, dwildt-
+//    $str_prompt = $str_prompt.'
+//      <div class="typo3-message message-warning">
+//        <div class="message-body">
+//          ' . $GLOBALS['LANG']->sL('LLL:EXT:quickshop_installer/lib/locallang.xml:promptSaveTwice'). '
+//        </div>
+//      </div>
+//    ';
+      // 120613, dwildt-
+      // Default prompt
+
+
+
+      /////////////////////////////////////////////////////////
+      //
+      // Get installed installer pages
+
+    $arr_installerPages = $this->get_installerPages();
+      // Get installed installer pages
+
+
+
+      /////////////////////////////////////////////////////////
+      //
+      // RETURN There is one installer page at least
+
+    if(!empty($arr_installerPages))
+    {
+      $str_installerPages = implode(null, $arr_installerPages);
+      $str_prompt = $str_prompt.'
+        <div class="typo3-message message-ok">
+          <div class="message-body">
+            ' . $GLOBALS['LANG']->sL('LLL:EXT:quickshop_installer/lib/locallang.xml:promptInstallPageExist'). '
+          </div>
+        </div>
+      ';
+      $str_prompt = $str_prompt.'
+        <div class="typo3-message message-information">
+          <div class="message-body">
+            ' . $GLOBALS['LANG']->sL('LLL:EXT:quickshop_installer/lib/locallang.xml:promptInstallNextSteps'). '
+          </div>
+        </div>
+      ';
+      $str_prompt = $str_prompt.'
+        <div class="typo3-message message-information">
+          <div class="message-body">
+            ' . $GLOBALS['LANG']->sL('LLL:EXT:quickshop_installer/lib/locallang.xml:promptDeleteInstallPage'). '
+          </div>
+        </div>
+      ';
+      $str_prompt = str_replace('###TITLE_UID###', $str_installerPages, $str_prompt);
+      return $str_prompt;
+    }
+      // RETURN There is one installer page at least
+
+
+
+      /////////////////////////////////////////////////////////
+      //
+      // RETURN There shouldn't install any installer page
+
+    if(strtolower($confArr['installPage']) == 'no' OR empty($confArr['installPage']))
+    {
+      $str_prompt = $str_prompt.'
+        <div class="typo3-message message-information">
+          <div class="message-body">
+            ' . $GLOBALS['LANG']->sL('LLL:EXT:quickshop_installer/lib/locallang.xml:promptEnableInstallPage'). '
+          </div>
+        </div>
+      ';
+      return $str_prompt;
+    }
+      // RETURN There shouldn't install any installer page
+
+
+
+      /////////////////////////////////////////////////////////
+      //
+      // Language configuration
+
+    switch($llStatic) 
+    {
+      case($llStatic == 'German'):
+        $this->str_llStatic = 'de';
+        break;
+      default:
+        $this->str_llStatic = 'en';
+    }
+      // Language configuration
+
+
+
+      /////////////////////////////////////////////////////////
+      //
+      // Insert page, TypoScript and plugin
+
+    $this->add_installerPage();
+    $this->add_installerTS();
+    $this->add_installerPlugin();
+      // Insert page, TypoScript and plugin
+
+
+
+      /////////////////////////////////////////////////////////
+      //
+      // RETURN page was added succesfully 
+
+    $arr_installerPages = $this->get_installerPages();
+    if(!empty($arr_installerPages))
+    {
+      $str_installerPages = implode(null, $arr_installerPages);
+      $str_prompt = $str_prompt.'
+        <div class="typo3-message message-ok">
+          <div class="message-body">
+            ' . $GLOBALS['LANG']->sL('LLL:EXT:quickshop_installer/lib/locallang.xml:promptInstallPageAdded'). '
+            ' . $GLOBALS['LANG']->sL('LLL:EXT:quickshop_installer/lib/locallang.xml:promptInstallNextSteps'). '
+          </div>
+        </div>
+      ';
+      $str_prompt = str_replace('###TITLE_UID###', $str_installerPages, $str_prompt);
+      return $str_prompt;
+    }
+      // RETURN page was added succesfully 
+
+
+
+      /////////////////////////////////////////////////////////
+      //
+      // RETURN with an error 
+
+    $str_prompt = $str_prompt.'
+      <div class="typo3-message message-error">
+        <div class="message-body">
+          ' . $GLOBALS['LANG']->sL('LLL:EXT:quickshop_installer/lib/locallang.xml:promptError'). '
+        </div>
+      </div>
+    ';
+    return $str_prompt;
+      // RETURN with an error 
+  }
+
+
+
+
+
+
+
+
+
+  /**
+ * add_installerPage(): Add a page with module 'quickshop_inst' to the root level
+ *
+ * @return  void
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+  private function add_installerPage()
+  {
+    $table                    = 'pages';
+    $int_maxUid               = $this->get_maxUid($table);
+    $this->int_pageUid        = $int_maxUid + 1;
+    $fields_values['uid']     = $this->int_pageUid;
+    $fields_values['title']   = 'Quick Shop Installer';
+    $fields_values['module']  = 'quickshop_inst';
+    $fields_values['sorting'] = '1000000000';
+    //var_dump(__METHOD__ . ' (' . __LINE__ . '): ' . $GLOBALS['TYPO3_DB']->INSERTquery($table,$fields_values,$no_quote_fields=FALSE));
+    //exit;
+    $GLOBALS['TYPO3_DB']->exec_INSERTquery($table,$fields_values,$no_quote_fields=FALSE);
+  }
+
+
+
+
+
+
+
+
+
+  /**
+ * add_installerPlugin(): Add the plugin
+ *
+ * @return  void
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+  private function add_installerPlugin()
+  {
+    $table      = 'tt_content';
+    $int_maxUid = $this->get_maxUid($table);
+    $int_maxUid = $int_maxUid + 1;
+    
+    $fields_values['uid']                 = $int_maxUid;
+    $fields_values['pid']                 = $this->int_pageUid;
+    $fields_values['CType']               = 'list';
+    $fields_values['header']              = '[Quick Shop Installer]';
+    $fields_values['header_layout']       = '100';
+    $fields_values['list_type']           = 'quickshop_installer_pi1';
+    //var_dump(__METHOD__ . ' (' . __LINE__ . '): ' . $GLOBALS['TYPO3_DB']->INSERTquery($table,$fields_values,$no_quote_fields=FALSE));
+    //exit;
+    $GLOBALS['TYPO3_DB']->exec_INSERTquery($table,$fields_values,$no_quote_fields=FALSE);
+  }
+
+
+
+
+
+
+
+
+
+  /**
+ * add_installerTS(): Add the TypoScript
+ *
+ * @return  void
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+  private function add_installerTS()
+  {
+    $table      = 'sys_template';
+    $int_maxUid = $this->get_maxUid($table);
+    $int_maxUid = $int_maxUid + 1;
+    
+    $fields_values['uid']                 = $int_maxUid;
+    $fields_values['pid']                 = $this->int_pageUid;
+    $fields_values['title']               = 'page_quickshopinstaller_' . sprintf('%03d', $int_maxUid);
+    $fields_values['root']                = '1';
+    $fields_values['clear']               = '3';
+    $fields_values['include_static_file'] = 'EXT:css_styled_content/static/';
+    $fields_values['config']              = '
+config {
+  baseURL            = ' . t3lib_div::getIndpEnv('TYPO3_REQUEST_HOST') . '/
+  language           = ' . $this->str_llStatic . '
+  htmlTag_langKey    = ' . $this->str_llStatic . '
+  metaCharset        = UTF-8
+  tx_realurl_enable  = 0
+  no_cache           = 1
+}
+page = PAGE
+page {
+  typeNum = 0
+  10 < styles.content.get
+}
+';
+    //var_dump(__METHOD__ . ' (' . __LINE__ . '): ' . $GLOBALS['TYPO3_DB']->INSERTquery($table,$fields_values,$no_quote_fields=FALSE));
+    //exit;
+    $GLOBALS['TYPO3_DB']->exec_INSERTquery($table,$fields_values,$no_quote_fields=FALSE);
+  }
+
+
+
+
+
+
+
+
+
+  /**
+ * get_installerPages(): Get all pages with module = quickshop_inst AND not deleted
+ *
+ * @return  array   rows with installer pages
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+  private function get_installerPages()
+  {
+    $rows           = null;
+    $select_fields  = 'uid, title';
+    $from_table     = 'pages';
+    $where_clause   = 'deleted = 0 AND module = "quickshop_inst"';
+    $groupBy        ='';
+    $orderBy        ='';
+    $limit          ='';
+    //var_dump(__METHOD__ . ' (' . __LINE__ . '): ' . $GLOBALS['TYPO3_DB']->SELECTquery($select_fields,$from_table,$where_clause,$groupBy,$orderBy,$limit));
+    $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select_fields,$from_table,$where_clause,$groupBy,$orderBy,$limit);
+    while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res))
+    {
+      $rows[] = '<li>' . $row['title'] . ' [uid: ' . $row['uid'] . ']</li>';
+      //$params['items'][] = array($row['itemValue'], $row['itemKey']);
+    }
+    return $rows;
+  }
+
+
+
+
+
+
+
+
+
+  /**
+ * get_maxUid(): Get the max uid of the given table
+ *
+ * @param    string       $from_table: the table
+ * @return  int   max uid
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+  private function get_maxUid($from_table)
+  {
+    $rows           = null;
+    $select_fields  = 'max(uid) AS maxUid';
+    $where_clause   = null;
+    $groupBy        = null;
+    $orderBy        = null;
+    $limit          = null;
+    //var_dump(__METHOD__ . ' (' . __LINE__ . '): ' . $GLOBALS['TYPO3_DB']->SELECTquery($select_fields,$from_table,$where_clause,$groupBy,$orderBy,$limit));
+    $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select_fields,$from_table,$where_clause,$groupBy,$orderBy,$limit);
+    $row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+    return $row['maxUid'];
+  }
+
+
+
+
+
+
+
+
+
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/quickshop_installer/lib/class.tx_quickshop_installer_extmanager.php'])
+{
+  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/quickshop_installer/lib/class.tx_quickshop_installer_extmanager.php']);
+}
+
+?>
