cc84c5f41711efdfede2d9a2fd3b91202903e63f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-14 21:02:25 +0000

    3.0.0 +++ #46270
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/quickshop_installer/trunk@72924 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ pi1/class.tx_quickshopinstaller_pi1_typoscript.php
@@ -0,0 +1,533 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010-2012 - Dirk Wildt <http://wildt.at.die-netzmacher.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ *
+ *
+ *  102: class tx_quickshopinstaller_pi1_typoscript extends tslib_pibase
+ *
+ *              SECTION: Main
+ *  160:     public function main( $content, $conf)
+ *
+ *              SECTION: Confirmation
+ *  243:     private function confirmation()
+ *
+ *              SECTION: Counter
+ *  319:     private function countPages( $pageUid )
+ *
+ *              SECTION: Create
+ *  348:     private function create( )
+ *  366:     private function createBeGroup()
+ *  472:     private function createContent()
+ *  614:     private function createFilesShop()
+ *
+ *              SECTION: Create pages
+ *  679:     private function createPages( )
+ *
+ *              SECTION: Create plugins
+ *  707:     private function createPlugins()
+ *
+ *              SECTION: Create records
+ *  949:     private function createRecordsPowermail()
+ * 1558:     private function createRecordsShop()
+ *
+ *              SECTION: Create TypoScript
+ * 1926:     private function createTyposcript()
+ *
+ *              SECTION: Consolidate
+ * 2233:     private function consolidatePageCurrent()
+ * 2456:     private function consolidatePluginPowermail()
+ * 2533:     private function consolidateTsWtCart()
+ *
+ *              SECTION: Extensions
+ * 2675:     private function extensionCheck( )
+ * 2740:     private function extensionCheckCaseBaseTemplate( )
+ * 2779:     private function extensionCheckExtension( $key, $title )
+ *
+ *              SECTION: Html
+ * 2820:     private function htmlReport( )
+ *
+ *              SECTION: Init
+ * 2877:     private function initBoolTopLevel( )
+ * 2918:     private function install( )
+ * 2957:     private function installNothing( )
+ *
+ *              SECTION: Prompt
+ * 2983:     private function promptCleanUp()
+ *
+ *              SECTION: ZZ
+ * 3032:     private function zz_getCHash($str_params)
+ * 3046:     public function zz_getMaxDbUid( $table )
+ * 3073:     private function zz_getPathToIcons()
+ * 3087:     private function zz_getFlexValues()
+ *
+ * TOTAL FUNCTIONS: 27
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
+ */
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+
+
+/**
+ * Plugin 'Quick Shop Inmstaller' for the 'quickshop_installer' extension.
+ *
+ * @author    Dirk Wildt <http://wildt.at.die-netzmacher.de>
+ * @package    TYPO3
+ * @subpackage    tx_quickshopinstaller
+ * @version 1.0.6
+ */
+class tx_quickshopinstaller_pi1_typoscript extends tslib_pibase
+{
+  public $prefixId      = 'tx_quickshopinstaller_pi1_typoscript';                // Same as class name
+  public $scriptRelPath = 'pi1/class.tx_quickshopinstaller_pi1_typoscript.php';  // Path to this script relative to the extension dir.
+  public $extKey        = 'quickshop_installer';                      // The extension key.
+
+  public $pObj = null;
+
+
+
+ /***********************************************
+  *
+  * Main
+  *
+  **********************************************/
+
+/**
+ * main( )
+ *
+ * @return  void
+ * @access  public
+ * @version 3.0.0
+ * @since   0.0.1
+ */
+  public function main( )
+  {
+    $records = array( );
+
+    $this->arrReport[ ] = '
+      <h2>
+       '.$this->pi_getLL('ts_create_header').'
+      </h2>';
+
+    $records = $this->records( );
+    $this->sqlInsert( $records );
+  }
+
+
+
+ /***********************************************
+  *
+  * Records
+  *
+  **********************************************/
+
+/**
+ * recordCaddy( )
+ *
+ * @return	array		$record : the TypoScript record
+ * @access  private
+ * @version 3.0.0
+ * @since   0.0.1
+ */
+  private function recordCaddy( $uid )
+  {
+    $record = null;
+    
+    $strUid = sprintf( '%03d', $uid );
+
+    $title = strtolower( $this->pi_getLL( 'page_title_caddy' ) );
+    $title = str_replace( ' ', null, $title );
+    $title = '+page_' . $title . '_' . $strUid;
+
+    $this->str_tsWtCart = $title;
+    $this->arr_tsUids[$this->str_tsWtCart]   = $uid;
+    
+    $record['title']               = $title;
+    $record['uid']                 = $uid;
+    $record['pid']                 = $this->arr_pageUids[$this->pi_getLL('page_title_caddy')];
+    $record['tstamp']              = time( );
+    $record['sorting']             = 256;
+    $record['crdate']              = time( );
+    $record['cruser_id']           = $this->markerArray['###BE_USER###'];
+    $record['include_static_file'] = ''.
+      'EXT:wt_cart/files/static/,' .
+      'EXT:powermail/static/pi1/,' .
+      'EXT:powermail/static/css_basic/';
+    // See $this->consolidateTsWtCart()
+    //$record['constants']           = '';
+    $record['config']           = '
+  //////////////////////////////////////////
+  //
+  // INDEX
+  //
+  // page
+  // plugin.tx_powermail_pi1
+  // plugin.tx_wtcart_pi1
+
+
+
+  //////////////////////////////////////////
+  //
+  // page
+
+page {
+  includeCSS {
+    // remove the cart default css
+    file3456     >
+  }
+}
+  // page
+
+
+
+  //////////////////////////////////////////
+  //
+  // plugin.tx_powermail_pi1
+
+plugin.tx_powermail_pi1 {
+  email {
+    sender_mail {
+      sender {
+        name {
+          value = Quick Shop
+        }
+        email {
+          value = ' . $this->markerArray['###MAIL_DEFAULT_RECIPIENT###'] . '
+        }
+      }
+    }
+  }
+  _LOCAL_LANG {
+    default {
+      locallangmarker_confirmation_submit = Order without costs and without delivering!
+    }
+    de {
+      locallangmarker_confirmation_submit = Kostenlos bestellen, nichts bekommen!
+    }
+  }
+}
+  // plugin.tx_powermail_pi1
+
+
+
+  //////////////////////////////////////////
+  //
+  // plugin.tx_wtcart_pi1
+
+plugin.tx_wtcart_pi1 {
+  _LOCAL_LANG {
+    default {
+      wtcart_ll_sku = Produkt-ID
+    }
+    de {
+      wtcart_ll_sku = Bestellnummer
+    }
+  }
+}
+  // plugin.tx_wtcart_pi1
+';
+    // Cart page
+
+    return $record;
+  }
+
+/**
+ * recordRoot( )
+ *
+ * @return	array
+ * @access  private
+ * @version 3.0.0
+ * @since   0.0.1
+ */
+  private function recordRoot( $uid )
+  {
+    $strUid = sprintf( '%03d', $uid );
+    $this->str_tsRoot = 'page_quickshop_' . $strUid;
+    $this->arr_tsUids[$this->str_tsRoot] = $uid;
+
+      // SWITCH : install case
+    switch( true )
+    {
+      case( $this->markerArray['###INSTALL_CASE###'] == 'install_all' ):
+        $record = $this->recordRootCaseAll( $uid );
+        break;
+      case( $this->markerArray['###INSTALL_CASE###'] == 'install_shop' ):
+        $record = $this->recordRootCaseShop( $uid );
+        break;
+    }
+      // SWITCH : install case
+    
+    return $record;
+  }
+
+/**
+ * recordRootCaseAll( )
+ *
+ * @return	array		$record : the TypoScript record
+ * @access  private
+ * @version 3.0.0
+ * @since   0.0.1
+ */
+  private function recordRootCaseAll( $uid )
+  {
+    $record = null;
+
+    $strUid = sprintf ('%03d', $uid);
+
+    $title  = strtolower( $GLOBALS['TSFE']->page['title'] );
+    $title  = str_replace( ' ', null, $title );
+    $title  = 'page_' . $title . '_' . $strUid;
+
+    $this->str_tsRoot = $title;
+    $this->arr_tsUids[$this->str_tsRoot] = $uid;
+
+    $record['title']                = $title;
+    $record['uid']                  = $uid;
+    $record['pid']                  = $GLOBALS['TSFE']->id;
+    $record['tstamp']               = time( );
+    $record['sorting']              = 256;
+    $record['crdate']               = time( );
+    $record['cruser_id']            = $this->markerArray['###BE_USER###'];
+    $record['sitetitle']            = $this->markerArray['###WEBSITE_TITLE###'];
+    $record['root']                 = 1;
+    $record['clear']                = 3;  // Clear all
+    $record['include_static_file']  = '' .
+      'EXT:css_styled_content/static/,EXT:base_quickshop/static/base_quickshop/,'.
+      'EXT:browser/static/,EXT:quick_shop/static/';
+    $record['includeStaticAfterBasedOn'] = 1;
+    $record['config']                    = ''.
+'config {
+  baseURL            = ' . $this->markerArray['###HOST###'] . '/
+  metaCharset        = UTF-8
+  tx_realurl_enable  = 0
+  no_cache           = 1
+  language           = ' . $GLOBALS['TSFE']->lang . '
+  htmlTag_langKey    = ' . $GLOBALS['TSFE']->lang . '
+}
+
+
+  ////////////////////////////////////////////////////////
+  //
+  // ajax page object
+
+  // Add this snippet into the setup of the TypoScript
+  // template of your page.
+  // Use \'page\', if the name of your page object is \'page\'
+  // (this is a default but there isn\'t any rule)
+
+[globalString = GP:tx_browser_pi1|segment=single] || [globalString = GP:tx_browser_pi1|segment=list] || [globalString = GP:tx_browser_pi1|segment=searchform]
+  page >
+  page < plugin.tx_browser_pi1.javascript.ajax.page
+[global]
+  // ajax page object
+
+  // TYPO3-Browser: ajax page object II. In case of localisation: Configure the id of sys_languagein the Constant Editor. Move in this line ...jQuery.default to ...jQuery.de (i.e.)
+browser_ajax < plugin.tx_browser_pi1.javascript.ajax.jQuery
+
+';
+
+    $record['constants'] = ''.
+'myConst {
+  //host = '.$this->markerArray['###HOST###'].'/
+  pages {
+    quick_shop = ' . $this->arr_pageUids[$GLOBALS['TSFE']->page['title']] . '
+    quick_shop {
+      cart      = ' . $this->arr_pageUids[$this->pi_getLL( 'page_title_caddy' )] . '
+      shipping  = ' . $this->arr_pageUids[$this->pi_getLL( 'page_title_shipping' )] . '
+      terms     = ' . $this->arr_pageUids[$this->pi_getLL( 'page_title_terms' )] . '
+      libraries = ' . $this->arr_pageUids[$this->pi_getLL( 'page_title_library' )].'
+      libraries {
+        header  = ' . $this->arr_pageUids[$this->pi_getLL( 'page_title_library_header' )].'
+        footer  = ' . $this->arr_pageUids[$this->pi_getLL( 'page_title_library_footer' )].'
+      }
+    }
+  }
+  paths {
+    res  = EXT:base_quickshop/res/
+    html = EXT:base_quickshop/res/html/
+    css  = EXT:base_quickshop/res/html/css/
+  }
+  files {
+    html {
+      template = index.html
+      css      = basic.css
+      favicon  = images/favicon.ico
+    }
+  }
+  dims {
+    header_image {
+      maxW = 210
+      maxH = 420
+    }
+  }
+  words {
+    // HTML a href title tag for menu item rootpage
+    title_tag_quick_shop_page = ' . $this->pi_getLL( 'phrases_ts_titleTag_quickshop_page' ) . '
+  }
+}';
+    $record['description'] = '// Created by QUICK SHOP INSTALLER at ' . date( 'Y-m-d G:i:s' );
+
+    $record['include_static_file'] = null .
+      'EXT:css_styled_content/static/,EXT:base_quickshop/static/base_quickshop/,' .
+      'EXT:browser/static/,EXT:quick_shop/static/';
+
+    return $record;
+  }
+
+/**
+ * recordRootCaseShopOnly( )
+ *
+ * @return	array		$record : the TypoScript record
+ * @access  private
+ * @version 3.0.0
+ * @since   0.0.1
+ */
+  private function recordRootCaseShopOnly( $uid )
+  {
+    $record = null;
+    
+    $strUid = sprintf( '%03d', $uid );
+
+    $title = strtolower( $GLOBALS['TSFE']->page['title'] );
+    $title = str_replace( ' ', null, $title );
+    $title = '+page_' . $title . '_' . $strUid;
+
+    $this->str_tsRoot = $title;
+    $this->arr_tsUids[$this->str_tsRoot] = $uid;
+
+    $record['title']                      = $title;
+    $record['uid']                        = $uid;
+    $record['pid']                        = $GLOBALS['TSFE']->id;
+    $record['tstamp']                     = time( );
+    $record['sorting']                    = 256;
+    $record['crdate']                     = time( );
+    $record['cruser_id']                  = $this->markerArray['###BE_USER###'];
+    $record['root']                       = 0;
+    $record['clear']                      = 0;  // Clear nothing
+    $record['includeStaticAfterBasedOn']  = 0;
+    $record['config']                     = ''.
+'config {
+  no_cache = 1
+}
+';
+    $record['constants']           = ''.
+'myConst {
+  //host = '.$this->markerArray['###HOST###'].'/
+  pages {
+    quick_shop = ' . $this->arr_pageUids[$GLOBALS['TSFE']->page['title']] . '
+    quick_shop {
+      cart      = ' . $this->arr_pageUids[$this->pi_getLL( 'page_title_caddy' )] . '
+      shipping  = ' . $this->arr_pageUids[$this->pi_getLL( 'page_title_shipping' )] . '
+      terms     = ' . $this->arr_pageUids[$this->pi_getLL( 'page_title_terms' )] . '
+    }
+  }
+  dims {
+    header_image {
+      maxW = 210
+      maxH = 420
+    }
+  }
+  words {
+    // HTML a href title tag for menu item rootpage
+    title_tag_quick_shop_page = ' . $this->pi_getLL( 'phrases_ts_titleTag_quickshop_page' ) . '
+  }
+}';
+
+    $record['description'] = '// Created by QUICK SHOP INSTALLER at ' . date( 'Y-m-d G:i:s' );
+
+    $record['include_static_file'] = null .
+      'EXT:css_styled_content/static/,EXT:browser/static/,EXT:quick_shop/static/';
+
+    return $record;
+  }
+
+/**
+ * records( )
+ *
+ * @return	array		$records : the TypoScript records
+ * @access  private
+ * @version 3.0.0
+ * @since   0.0.1
+ */
+  private function records( )
+  {
+    $records  = array( );
+    $uid      = $this->zz_getMaxDbUid( 'sys_template' );
+    
+      // TypoScript for the root page
+    $uid = $uid + 1;
+    $records[$uid] = $this->recordRoot( $uid );
+    
+      // TypoScript for the caddy page
+    $uid = $uid + 1;
+    $records[$uid] = $this->recordCaddy( $uid );
+
+    return $records;
+  }
+
+
+
+ /***********************************************
+  *
+  * Sql
+  *
+  **********************************************/
+
+/**
+ * sqlInsert( )
+ *
+ * @param   array   $records : TypoScript records for pages
+ * @return  void
+ * @access  private
+ * @version 3.0.0
+ * @since   0.0.1
+ */
+  private function sqlInsert( $records )
+  {
+    foreach( $records as $record )
+    {
+      //var_dump($GLOBALS['TYPO3_DB']->INSERTquery($table, $record, $no_quote_fields));
+      $GLOBALS['TYPO3_DB']->exec_INSERTquery( 'sys_template', $record );
+      $marker['###TITLE###']     = $record['title'];
+      $marker['###UID###']       = $record['uid'];
+      $marker['###TITLE_PID###'] = '"' . $this->arr_pageTitles[$record['pid']] .
+                                   '" (uid ' . $record['pid'] . ')';
+      $prompt = '
+        <p>
+          '.$this->arr_icons['ok'] . ' ' . $this->pi_getLL( 'ts_create_prompt' ).'
+        </p>';
+      $prompt = $this->cObj->substituteMarkerArray( $prompt, $marker );
+      $this->arrReport[ ] = $prompt;
+    }
+  }
+}
+
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/quickshop_installer/pi1/class.tx_quickshopinstaller_pi1_typoscript.php'])
+{
+  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/quickshop_installer/pi1/class.tx_quickshopinstaller_pi1_typoscript.php']);
+}
