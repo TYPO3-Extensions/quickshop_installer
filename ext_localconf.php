330c618210388350bf4fb305b97bea900b56d82c
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-15 16:42:47 +0000

    3.0.0 +++ #46270
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/quickshop_installer/trunk@72990 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,7 +8,7 @@ if (!defined ('TYPO3_MODE'))  die ('Access denied.');
   //
   // Extending TypoScript from static template uid=43 to set up userdefined tag
 
-t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_qsinstaller_pi1.php','_pi1','list_type',1);
+t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi1/class.tx_quickshopinstaller_pi1.php', '_pi1', 'list_type', 1 );
   // Extending TypoScript from static template uid=43 to set up userdefined tag
 
 
