81e2353801436ef5a2efbbcc1aa92e6164170e66
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-23 12:06:09 +0000

    3.0.6 +++ LLL
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/quickshop_installer@78211 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/lib/class.tx_quickshop_installer_extmanager.php
+++ trunk/lib/class.tx_quickshop_installer_extmanager.php
@@ -84,7 +84,7 @@ class tx_quickshop_installer_extmanager
     //$confArr    = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['quickshop_installer']);
     // 120613, dwildt, 1+
     $confArr    = $_POST['data'];
-var_dump( $_POST['data'] );
+    //var_dump( $_POST['data'] );
     $llStatic   = $confArr['LLstatic'];
 
 
