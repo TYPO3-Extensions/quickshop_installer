9b9de36cb9a9ec30ab9fb146334f8c0a94a6035f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-11-25 01:03:50 +0000

    6.0.0 +++ locallang
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/quickshop_installer@86994 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/pi1/flexform_locallang.php
+++ trunk/pi1/flexform_locallang.php
@@ -12,7 +12,7 @@ $LOCAL_LANG = array(
     'sheet_install.host'                    => 'The host of your shop (with http://...). Leave it empty, if the frontend URL should used. ',
     'sheet_install.website_title'           => 'Title of your website.',
     'sheet_install.mail_subject'            => 'Subject of confirmation mails - required',
-    'sheet_install.mail_default_recipient'  => 'E-mail of the shop owner (is the sender of e-mails to customers) - required',
+    'sheet_install.mail_default_recipient'  => 'E-mail of the shop owner (receives e-mail) - required',
   ),
   'de' => array(
     'sheet_install'                         => 'Installation',
@@ -25,7 +25,7 @@ $LOCAL_LANG = array(
     'sheet_install.backend_user'            => 'Backend Benutzer (wird der Eigentuemer aller neuen Seiten des Quick Shops)',
     'sheet_install.host'                    => 'Der Host Deines Shops (mit http://...). Lass das Feld leer, wenn Du die Frontend URL verwenden willst. ',
     'sheet_install.mail_subject'            => 'Betreff der Bestaetigungs-Mails - Pflichtfeld',
-    'sheet_install.mail_default_recipient'  => 'Mail des Eigentuemers des Shops (das ist auch die Absender-Mail fuer Mails an Kunden) - Pflichtfeld',
+    'sheet_install.mail_default_recipient'  => 'Mail des Eigentuemers des Shops (bekommt E-Mails) - Pflichtfeld',
   ),
 );
 ?>
